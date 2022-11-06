2b966761ab98069decd97242db56fdfb2c3bb162
Alex Alcala <magnethus@gmail.com>
2022-11-06 05:04:14 -0400

    Updates

---
+++ precache-manifest.d9a342f9375fa50e5a1db59ca0e3a4ab.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5213a3d3f1227add16cb1fc022cdc53",
+    "url": "/makingAPIRequestForMovies/index.html"
+  },
+  {
+    "revision": "877b3fe9422c5130f6c5",
+    "url": "/makingAPIRequestForMovies/static/css/main.0a9def36.chunk.css"
+  },
+  {
+    "revision": "a82b4f4a334af60b9ca9",
+    "url": "/makingAPIRequestForMovies/static/js/2.323ff2c8.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/makingAPIRequestForMovies/static/js/2.323ff2c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "877b3fe9422c5130f6c5",
+    "url": "/makingAPIRequestForMovies/static/js/main.f1441761.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/makingAPIRequestForMovies/static/js/main.f1441761.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f31636fc07a2b9a3d6d",
+    "url": "/makingAPIRequestForMovies/static/js/runtime-main.1a671996.js"
+  }
+]);
